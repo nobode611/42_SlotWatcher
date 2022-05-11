a1423354bc36da529c13711e538e686ca7214232
tratanat <tawan.rtn@gmail.com>
2022-05-11 19:16:41 +0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # 42_SlotWatcher
-Small applet to watch for open evaluation slots for 42 projects.
+## Description
+A very small applet to look for available evaluation slots for School 42's projects. It makes a request to the Intra server at a customizable interval and will push a Windows notification if it finds available slots. Your mileage may vary due to the way the system works. The moment you refresh or enter the evaluation page, you might not find the slot, but it should still be more convenient than continuously pressing F5 to find some open slots.
+
+It's made quickly, so it might not work properly. Use at your own risk.
+
+![Notification Example](imgs/notification.png "Sometimes F5 is just too much")
+
+## Usage
+The application reads a config.json file for information. If the file doesn't exist, it will prompt for the following information:
+
+* Project ID: the name of the project as written in the URL
+
+* Team ID: team ID as written in the URL
+
+<p align="center">https&colon;//projects.intra&period;42&period;fr/projects/ <font color="red"><strong>Project ID</strong></font> /slots?team_id= <font color="red"><strong>Team ID</font></strong></p>
+
+* Session ID: the cookie of the your intra login. It's found on the request's header in the project's open slot page. It should be in the format: `"_intra_42_session_production=**Session ID**"`
+
+![Cookie Example](imgs/cookie.png)
+
+## Flags
+* `-refresh=120` - Set the interval between each refresh in seconds. Default is 120 seconds.
+* `-days=2` - Set the range that the program will look for available slots counting from today. Default is 2 days.
+* `-help`
---
+++ watcher.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"gopkg.in/toast.v1"
+)
+
+type Configuration struct {
+	ProjectId string
+	TeamId    string
+	SessionId string
+}
+
+type OpenSlot struct {
+	end   string
+	start string
+	title string
+	id    string
+	ids   string
+}
+
+func main() {
+	var refreshRate int
+	var daysOffset int
+
+	flag.IntVar(&refreshRate, "refresh", 120, "Time between refresh in seconds.")
+	flag.IntVar(&daysOffset, "days", 2, "Range of days to look for available slots.")
+	flag.Parse()
+
+	var configuration Configuration
+
+	fmt.Printf("-------------------------\n")
+	fmt.Printf("-    42 Slot Watcher    -\n")
+	fmt.Printf("-------------------------\n")
+
+	_, err := os.Stat("config.json")
+	if err != nil {
+		getInfo(&configuration)
+	}
+	content, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = json.Unmarshal(content, &configuration)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	notice := toast.Notification{
+		AppID:   "42 Slot Watcher",
+		Title:   "Slot(s) Available",
+		Message: fmt.Sprintf("Found available slot(s) for project %s", configuration.ProjectId),
+		Actions: []toast.Action{
+			{"protocol", "Open Page", fmt.Sprintf("https://projects.intra.42.fr/projects/%s/slots?team_id=%s", configuration.ProjectId, configuration.TeamId)},
+		},
+	}
+
+	var openSlot []OpenSlot
+	var slots []byte
+
+	fmt.Printf("Project ID: %s Team ID: %v\n", configuration.ProjectId, configuration.TeamId)
+
+	for {
+		slots = getSlots(daysOffset, configuration.ProjectId, configuration.TeamId, configuration.SessionId)
+		err := json.Unmarshal(slots, &openSlot)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if len(openSlot) > 0 {
+			log.Println("Found available slots.")
+			err := notice.Push()
+			if err != nil {
+				log.Fatalln(err)
+			}
+		} else {
+			log.Println("No slot available.")
+		}
+		time.Sleep(time.Duration(refreshRate) * time.Second)
+	}
+}
+
+func getInfo(configuration *Configuration) {
+	fmt.Println("Config file not found. Please enter information")
+	fmt.Print("Project ID: ")
+	fmt.Scanln(configuration.ProjectId)
+	fmt.Print("Team ID: ")
+	fmt.Scanln(configuration.TeamId)
+	fmt.Print("Session ID: ")
+	fmt.Scanln(configuration.SessionId)
+	config, err := os.OpenFile("config.json", os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	config.Close()
+	content, err := json.Marshal(configuration)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = ioutil.WriteFile("config.json", content, 0644)
+}
+
+func getSlots(daysOffset int, projectId, teamId, sessionId string) []byte {
+	currentTime := time.Now()
+	startDate := currentTime.Format("2006-02-05")
+	currentTime = currentTime.AddDate(0, 0, daysOffset)
+	endDate := currentTime.Format("2006-02-05")
+
+	client := &http.Client{}
+	url := fmt.Sprintf("https://projects.intra.42.fr/projects/%s/slots.json?team_id=%s&start=%s&end=%s", projectId, teamId, startDate, endDate)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("cookie", fmt.Sprintf("_intra_42_session_production=%s", sessionId))
+	req.Host = "projects.intra.42.fr"
+	res, err := client.Do(req)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer res.Body.Close()
+
+	var resBody []byte
+	if res.StatusCode == http.StatusOK {
+		resBody, err = io.ReadAll(res.Body)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		log.Fatalf("%v Response from server\n", res.StatusCode)
+	}
+	return resBody
+}
